c6be0b23dfd3dd09485e500e68a84c9c2534d6bc
sabbir hossain <mdsabbir477470@gmail.com>
2022-08-21 13:02:04 +0600

    money master

---
+++ script.js
@@ -0,0 +1,12 @@
+/* ................ common function .............  */
+
+
+//step-1:...........
+document.getElementById('calculate-btn').addEventListener('click', function () {
+    // step-2:......
+    const incomeNumber = document.getElementById('income-field');
+    const inputField = document.getElementById('income-field');
+    const inputFieldString = inputField.value;
+
+    // step-3:.........
+})
